bb4e1d1c0fef959bf74fe15163e2d4558ddf32ba
rahulhere-1 <rmendigeri4@gmail.com>
2025-08-17 13:40:47 +0530

    added circuit breaker for services

--- api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +12,26 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes().route(
+				p -> p.path("/site/orders/**")
+						.filters(f -> f.rewritePath("/site/orders/(?<segment>.*)","/${segment}")
+								.circuitBreaker(config -> config.setName("ordersCircuitBreaker")))
+						.uri("lb://ORDERS"))
+				.route(
+				p -> p.path("/site/inventory/**")
+						.filters(f -> f.rewritePath("/site/inventory/(?<segment>.*)","/${segment}")
+								.circuitBreaker(config -> config.setName("inventoryCircuitBreaker")))
+						.uri("lb://INVENTORY"))
+				.route(
+				p -> p.path("/site/product/**")
+						.filters(f -> f.rewritePath("/site/product-service/(?<segment>.*)","/${segment}"))
+						.uri("lb://PRODUCT"))
+						.build();
+
+	}
+
 
 }
--- order-service/src/main/java/com/example/order_service/service/InventoryClient.java
+++ order-service/src/main/java/com/example/order_service/service/InventoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("inventory-service")
+@FeignClient("inventory")
 public interface InventoryClient {
 
     @GetMapping("/api/inventory/isAvailable")
